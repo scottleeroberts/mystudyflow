12cfa9abea61618a16e8aaa97404a5140ff46948
Scott Roberts <scott.lee.roberts@gmail.com>
2023-09-10 22:00:00 +0530

    Pomodoro version 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Pomodoro from './components/Pomodoro.vue'
 </script>
 <template>
-  <HelloWorld/>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Pomodoro</h5>
+    <p class="card-text">One at at time...</p>
+    <Pomodoro />
+  </div>
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
